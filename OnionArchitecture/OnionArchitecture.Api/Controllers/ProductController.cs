af676433d6d41ae942aeb7148755bd6b6a10271b
Nadeau, Danick (Consultant) <Danick.Nadeau-c@transports.gouv.qc.ca>
2024-10-23 10:49:11 -0400

    - Added api project for the Onion Architecture example.

---
+++ OnionArchitecture/OnionArchitecture.Api/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.Core.Enums;
+using OnionArchitecture.Core.Interfaces;
+
+namespace OnionArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductServices _productServices;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(
+            IProductServices productServices,
+            ILogger<ProductController> logger)
+        {
+            _productServices = productServices;
+            _logger = logger;   
+
+        }
+
+        [HttpPost("Buy")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult BuyProduct(ProductType product)
+        {
+            try
+            {   
+                var bought = _productServices.BuyProducts(product);
+
+                return Ok(bought);
+            }
+            catch (Exception ex)
+            {
+                return UnexpectedErrorHandling(ex);
+            }
+        }
+
+        private IActionResult UnexpectedErrorHandling(Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occured");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
